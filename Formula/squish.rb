a24e50a3f8cd62518fdbe26163439b90e8017fb5
mahesh jamdade <maheshmn121@gmail.com>
2026-09-06 19:27:17 -0400

    hash fix

--- Formula/squish.rb
+++ Formula/squish.rb
@@ -4,7 +4,7 @@ class Squish < Formula
   # After tagging a release (e.g. v0.1.0), point url at its tarball and fill in
   # the sha256:  curl -sL <url> | shasum -a 256
   url "https://github.com/maheshj01/squish/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 01c801959bb862b5ab5aff0679942abe4b50fd7891934f5379776297a754b774"
+  sha256 "01c801959bb862b5ab5aff0679942abe4b50fd7891934f5379776297a754b774"
   license "MIT"
   head "https://github.com/maheshj01/squish.git", branch: "main"
 
